dc35706f3d25dd1dc6f911abb145cbf7535c0aa9
rioopr-beep <riokmxl@gmail.com>
2026-04-11 23:08:46 +0700

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,20 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
     const gallery = document.querySelector('.gallery-container');
-    
-    // Daftar nama file foto Anda (sesuaikan dengan jumlah dan nama file di assets/)
-    const photoList = ['1.webp', '2.webp', '3.webp'];
+    const photoList = [
+        '1.1775917841-picsay.webp',
+        '2.1775917841-pics.webp'
+    ];
 
     photoList.forEach(fileName => {
         const img = document.createElement('img');
         img.src = `assets/${fileName}`;
         img.classList.add('rp-img');
-        img.setAttribute('alt', 'Project RP Visual');
         
-        // Memasukkan ke dalam galeri di index.html
+        img.onload = () => {
+            img.classList.add('loaded');
+        };
+
         if (gallery) {
             gallery.appendChild(img);
         }
     });
-
-    console.log("RP-System: Engine Aktif, Aset dimuat.");
 });
